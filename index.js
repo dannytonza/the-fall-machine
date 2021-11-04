0fde5d8e5935db205cde63cef879fb82ec463706
Dan Howell <danielhowell1991@gmail.com>
2021-11-03 21:49:29 -0500

    add lyrics

---
+++ index.js
@@ -0,0 +1,49 @@
+const PROJECT_NAME = 'The Fall Lyric Generator';
+
+const lyrics = [
+    { song: "Frightened", lyric: "Someone's always on my tracks." },
+    {
+        song: "Bill Is Dead",
+        lyric:
+            "These are the finest of my life / This is the greatest time of my life."
+    },
+    {
+        song: "Mountain Energy",
+        lyric:
+            "Dolly Parton and Lord Byron / They said patriotism is the last refuge / but now it’s me."
+    },
+    {
+        song: "Totally Wired",
+        lyric: "You don’t have to be an American to be strange."
+    },
+    {
+        song: "Garden",
+        lyric: "Roads. Had forgot what other’s still try to grasp."
+    },
+    {
+        song: "Dr Bucks Letter",
+        lyric:
+            "To cheer myself up / Put the radio on, get the magazine out / And read about the essence of Tong."
+    },
+    {
+        song: "New Puritan",
+        lyric:
+            "Over imported trees / All the film ghosts will rise up / With the sexually abused and the new youth / In Britain the scream of electric pumps in a renovated pub."
+    },
+    {
+        song: "Oswald Defence Lawyer",
+        lyric:
+            "He’s liberal and insane, he caught the good news horse / His opposite is vain, the cardboard fake in the witness stand."
+    },
+    {
+        song: "How I Wrote Elastic Man",
+        lyric:
+            "The Observer magazine just about sums him up / E.g. self-satisfied, smug."
+    },
+
+    {
+        song: "Before the Moon Falls",
+        lyric:
+            "Up here in the North there’s no wage packet jobs for us, thank Christ."
+    }
+];
